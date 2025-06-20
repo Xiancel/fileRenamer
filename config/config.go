396f63e00bf4dc7f3d9c41065d9bdc10a4b1d8f4
Andrey <medvedevandrey860@gmail.com>
2025-06-20 16:03:48 +0300

    stage 2.3 uptade config

--- config/config.go
+++ config/config.go
@@ -26,11 +26,11 @@ func Inputs() {
 	dir := strings.TrimSpace(getInput("> "))
 
 	// приймає шаблон від користувача
-	fmt.Println("Вкажіть шаблон файлів (наприклад, *.jpg):")
+	fmt.Println("\nВкажіть шаблон файлів (наприклад, *.jpg):")
 	templ := strings.TrimSpace(getInput("> "))
 
 	// приймає дію від користувача
-	fmt.Println("Оберіть дію (prefix, suffix, replace, extension, lowercase, uppercase):")
+	fmt.Println("\nОберіть дію (prefix, suffix, replace, extension, lowercase, uppercase):")
 	action := getInput("> ")
 	action = strings.TrimSpace(action) // видалення зайвих пробілів
 
@@ -50,19 +50,19 @@ func ValueToAdd(action string) string {
 	var input string
 	switch action {
 	case "prefix":
-		fmt.Println("Введіть значення для додавання як префікс:")
+		fmt.Println("\nВведіть значення для додавання як префікс:")
 		input = getInput("> ")
 	case "suffix":
-		fmt.Println("Введіть значення для додавання як суфікс:")
+		fmt.Println("\nВведіть значення для додавання як суфікс:")
 		input = getInput("> ")
 	case "replace":
-		fmt.Println("Введіть значення для замінни(через пробіл, наприклад text new):")
+		fmt.Println("\nВведіть значення для замінни(через пробіл, наприклад text new):")
 		input = getInput("> ")
 	case "extension":
-		fmt.Println("Введіть значення для змінненя розширення(без крапки):")
+		fmt.Println("\nВведіть значення для змінненя розширення(без крапки):")
 		input = getInput("> ")
 	default:
-		fmt.Println("Такой дії не існує")
+		fmt.Println("\nТакой дії не існує")
 		return ""
 	}
 	return input
